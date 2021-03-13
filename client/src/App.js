8d26263ee9c52e885cdc4f73bf7d09553c67d102
MAHE <divyamanipal01@gmail.com>
2021-03-13 10:38:17 +0530

    Modified files

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import CreatePost from './components/createPost';
+import PostList from './components/postsList';
+import PostDetails from './components/postDetails';
+import UpdatePost from './components/updatePost';
+
+class App extends Component{
+  render(){
+    return(
+      <Router>
+        <div>
+          <Route exact path='/' component={PostList}/>
+          <Route path='/create-post' component={CreatePost}/>
+          <Route path='/edit-post/:id' component={UpdatePost}/>
+          <Route path='/show-post/:id' component={PostDetails}/>
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
+
